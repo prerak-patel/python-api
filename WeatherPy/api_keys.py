49dd7ff9a94eb0aacbc1475480f5b53eda67a34d
prerak-patel <emailprerak@gmail.com>
2020-03-12 16:30:36 -0500

    Adding OpenWeatherMap and GoogleApiKey

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "803ebdb44283a29701ac5a48a55987ad"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBJfAquvNwwo4a0Nv-CbgC9W2eycB_gEAY"
